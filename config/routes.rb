bca3d1a6f039f5bfc10176bcd37425111adcfc01
Casey Ann Cumbow <cumbow8@gmail.com>
2016-10-30 20:01:58 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "pokemons#index"
-  resources :captures, only: [:create]
+  resources :backpacks, only: [:create]
   resources :games, only: [:create]
 end
